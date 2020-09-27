d8df4a99082c98984204208a839a727072895529
aura6601 <55701863+aura6601@users.noreply.github.com>
2020-09-27 13:42:12 +0300

    Add files via upload

--- v.cpp
+++ v.cpp
@@ -18,17 +18,28 @@ struct studentas {
 
 int main()
 {
-    studentas eilute; 
+    studentas eilute; studentas eilute_mas[5];
     cout << "Iveskite studentu skaiciu: \n ";
     int n = 0;
     cin >> n;
     studentas* grupe = new studentas[n];
+    std::vector<studentas> Eil_vect;
    
     cout << "Iveskite reikalingus duomenis (Vardas, Pavarde, egz. paz.,5 nd. paz.):\n";
     for (int i = 0; i < n; i++) {
         cout << i + 1 << "-ojo studento duomenys: ";
         cin >> grupe[i].vardas >> grupe[i].pavarde >> grupe[i].egz;
+        for (int i = 0; i < 5; i++) {
+            cin >> eilute.pazymiai[i];
+            eilute.gal = eilute.gal + (float)eilute.pazymiai[i];
+        }
+        eilute.gal = eilute.gal / 5.0;
+        eilute.gal = eilute.gal * 0.4 * 0.6 * grupe[i].egz;
+        cout << "Ivesti duomenys: " << grupe[i].vardas << " " << grupe[i].pavarde << " " << grupe[i].egz;
+        for (int i = 0; i < 5; i++) cout << " " << eilute.pazymiai[i];
+        cout << " " << eilute.gal << std::endl;
     }
+
     for (int i = 0; i < n; i++) {
         cout << grupe[i].vardas << " : ";
     }
