0e1781cc75def8599e73a9d0bd3bbe9a1a043a24
aura6601 <55701863+aura6601@users.noreply.github.com>
2020-10-01 16:04:40 +0300

    Add files via upload

--- galutinis.cpp
+++ galutinis.cpp
@@ -1,3 +1,4 @@
+
 // v.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
@@ -8,22 +9,25 @@
 #include <numeric>
 #include <iomanip>
 #include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cctype>
+#include <iostream>
+#include <cstring>
 
 using std::cout;
 using std::cin;
 using std::string;
 using std::endl;
-using std::vector; 
-using std::streamsize; 
+using std::vector;
 
+double rezultatai(int egzaminas, vector<int> pazymiai);
+void galutinis(vector<string> vardai, vector<string> pavardes, vector<double> galutinis);
 
-struct studentas {
-    string vardas, pavarde;
-    std::vector<int> pazymiai;
-    float egz;
-    float gal = 0;
+int egzaminas, nd;
+double galutinis;
+char vidurkis;
 
-};
 float vidurkis(vector<int> pazymiai) {
     int suma = 0;
     int dydis = pazymiai.size();
@@ -36,38 +40,48 @@ float vidurkis(vector<int> pazymiai) {
 
 int main()
 {
-    studentas eilute; studentas eilute_mas[5];
+    vector<std::string> vardai;
+    vector<std::string> pavardes;
+    vector<double> pazymiai;
+    vector<double> galutinis;
+    string vardas;
+    string pavarde;
+    
     cout << "Iveskite studentu skaiciu: \n ";
     int n = 0;
     cin >> n;
-    studentas* grupe = new studentas[n];
-    std::vector<studentas> Eil_vect;
-   
-    cout << "Iveskite reikalingus duomenis (Vardas, Pavarde, egz. paz.,5 nd. paz.):\n";
-    for (int i = 0; i < n; i++) {
-        cout << i + 1 << "-ojo studento duomenys: ";
-        cin >> grupe[i].vardas >> grupe[i].pavarde >> grupe[i].egz;
-        for (int i = 0; i < 5; i++) {
-            cin >> eilute.pazymiai[i];
+    if (n > 0)
+    {
+        cout << "Iveskite reikalingus duomenis (Vardas, Pavarde, egz. paz.,5 nd. paz.):\n";
+        for (int i = 0; i < n; i++) {
+            cout << i + 1 << "-ojo studento duomenys: ";
+            cin >> grupe[i].vardas >> grupe[i].pavarde >> grupe[i].egz;
+            for (int i = 0; i < 5; i++) {
+                cin >> eilute.pazymiai[i];
+            }
+            streamsize prec = cout.precision();
+            cout << "Ivesti duomenys: " << grupe[i].vardas << " " << grupe[i].pavarde << " " << grupe[i].egz << setprecision(3)
+                << 0.6 * grupe[i].egz + 0.4 * suma / n << setprecision(prec) << endl;
+            for (int i = 0; i < 5; i++) cout << " " << eilute.pazymiai[i];
+            cout << " " << grupe[i].gal << std::endl;
         }
-        streamsize prec = cout.precision();
-        cout << "Ivesti duomenys: " << grupe[i].vardas << " " << grupe[i].pavarde << " " << grupe[i].egz << setprecision(3)
-            << 0.6 * grupe[i].egz + 0.4 * suma / n << setprecision(prec) << endl;
-        for (int i = 0; i < 5; i++) cout << " " << eilute.pazymiai[i];
-        cout << " " << grupe[i].gal << std::endl;
-    }
 
-    for (int i = 0; i < n; i++) {
-        cout << grupe[i].vardas << " : ";
-    }
-    cout << std::endl; 
-    studentas* temp = new studentas[n + 1];
-    for (int i = 0; i < n; i++) temp[i] = grupe[i];
+        for (int i = 0; i < n; i++) {
+            cout << grupe[i].vardas << " : ";
+        }
+        cout << std::endl;
+        studentas* temp = new studentas[n + 1];
+        for (int i = 0; i < n; i++) temp[i] = grupe[i];
 
-    for (int i = 0; i < n; i++) {
-        cout << temp[i].vardas << " : ";
+        for (int i = 0; i < n; i++) {
+            cout << temp[i].vardas << " : ";
+        }
+        cout << std::endl;
     }
-    cout << std::endl; 
+    else  std::cout << "Studentu skaicius turi buti teigiamas sveikasis skaicius \n";
+           
     
 
+
 }
+
